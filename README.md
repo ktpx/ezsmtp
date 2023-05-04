8baa8e8758e564e201a4b39bb4f3e76f8945e416
ktpx <na>
2023-05-04 19:09:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# go ezsmtp
+
+Easy smtp forwarder
+
+
---
+++ ezsmtp.go
@@ -0,0 +1,62 @@
+//
+//  EZSMTP -- easy smtp mail forwarder
+//
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/smtp"
+)
+
+// Command line options
+type Opts struct {
+	Subject string
+	Sender  string
+	To      string
+	Cc      string
+	Body    string
+	Server  string
+	Port    string
+}
+
+var opt Opts
+
+func init() {
+	flag.StringVar(&opt.Subject, "s", "(No Subject)", "Mail `Subject`")
+	flag.StringVar(&opt.To, "t", "", "`Email` Receipient`")
+	flag.StringVar(&opt.Body, "m", "", "`Message` Body")
+	flag.StringVar(&opt.Cc, "cc", "", "`Email` CC to Receipients`")
+	flag.StringVar(&opt.Server, "h", "localhost", "`smtp` server")
+	flag.StringVar(&opt.Port, "p", "25", "`smtp port` port")
+	flag.StringVar(&opt.Sender, "f", "ezsmtp@localhost", "`Sender` email")
+}
+
+func main() {
+
+	flag.Parse()
+
+    // TODO: add auth
+	// Set up authentication information.
+	// auth := smtp.PlainAuth("", "user@example.com", "password", "mail.example.com")
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+	
+	if len(opt.To) == 0 {
+		log.Fatal("No recipient specified.")
+	}
+	if len(opt.Body) == 0 {
+		log.Fatal("No message specified.")
+	}	
+	to := []string{opt.To}
+	header := fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s", opt.To, opt.Subject, opt.Body)
+	msg := []byte(header)
+
+	err := smtp.SendMail(opt.Server+":"+opt.Port, nil, opt.Sender, to, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Sending mail ...", to)
+}
